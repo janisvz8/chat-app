42ac01a867648e7ec9f5fd1eb2710dbcae430561
janis_vaz_adrosonic <janis.vaz@adrosonic.com>
2018-09-07 12:39:55 +0530

    cors changes1

--- server.js
+++ server.js
@@ -2,31 +2,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var cors = require('cors');
+
+app.use(cors()); 
+app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
     res.sendFile(__dirname + '/public/index.html')
-});
-
-app.use(express.static('public'));
-
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
 });
 
 users = [];
