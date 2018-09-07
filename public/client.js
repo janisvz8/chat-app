40586c8016f4313ef591fb37e821d2f635237844
janis_vaz_adrosonic <janis.vaz@adrosonic.com>
2018-09-07 12:16:52 +0530

    first commit

---
+++ public/client.js
@@ -0,0 +1,42 @@
+// initializing socket, connection to server
+var socket = io.connect('http://localhost:3001');
+socket.on('connect', function(data) {
+    socket.emit('join', 'Hello server from client');
+});
+
+function setUsername() {
+    socket.emit('setUsername', document.getElementById('name').value);
+};
+
+$('#chatpage').hide();
+
+var user;
+    socket.on('userExists', function(data) {
+        document.getElementById('error-container').innerHTML = data;
+    });
+
+    socket.on('userSet', function(data) {
+        user = data.username;
+        console.log(user);
+        $('#myForm').prepend('<div id="thread"></div>');
+        $('#intro').hide();
+        $('#chatpage').show();
+     });
+
+// listener for 'thread' event, which updates messages
+socket.on('thread', function(data) {
+    $('#thread').append('<div><b>' + data.user + '</b>: ' + data.message + '</div>');
+});
+
+socket.on('broadcast',function(data) {
+    //document.getElementById('users').innerHTML = data.description;
+    $('#users').append('<a href="#!" class="collection-item"><span class="online"></span>' + data.description + '</a>');
+ });
+
+// sends message to server, resets & prevents default form action
+$('form').submit(function() {
+    var message = $('#message').val();
+    socket.emit('messages', {message: message, user: user});
+    this.reset();
+    return false;
+});
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res, next) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.use(express.static('public'));
+
+users = [];
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    socket.on('setUsername', function(data) {
+        console.log(users);
+        if(users.indexOf(data) > -1) {
+            socket.emit('userExists', data + ' username is taken! Try some other username.');
+            
+        } else {
+            users.push(data); 
+            io.sockets.emit('broadcast',{ description: data });
+            socket.emit('userSet', {username: data});
+        }
+    })
+    //io.sockets.emit('broadcast',{ description: clients + ' clients connected!'});
+    socket.on('disconnect', function(data) {
+        console.log('disconnected');
+        io.sockets.emit('broadcast',{ description: data + 'disconnected!'});
+    });
+
+    socket.on('messages', function(data){
+        socket.emit('thread', data);
+        socket.broadcast.emit('thread', data);
+    });
+});
+
+server.listen(3001);
